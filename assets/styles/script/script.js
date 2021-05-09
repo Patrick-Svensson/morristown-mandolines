5c10ce5105bb6e1e83fd3522f20c49cc70062195
Patrick Svensson <ps.digitalepost@gmail.com>
2021-05-10 00:00:17 +0200

    Add animation to menu. Change font.

--- assets/styles/script/script.js
+++ assets/styles/script/script.js
@@ -1,11 +1,20 @@
 // ***** Dropdown Menu *****
 
+const slideInMenu = document.querySelector(".navbar__toggler");
+const slideOutMenu = document.querySelector(".navbar__link--close");
+const close = document.querySelector("#navDropdownMenu");
+
+slideInMenu.addEventListener("click", () => {
+    document.querySelector(".navbar__link-wrapper").classList.remove("animate__slideOutRight");
+    document.querySelector(".navbar__link-wrapper").classList.add("animate__slideInRight");
+});
+
+slideOutMenu.addEventListener("click", () => {
+    document.querySelector(".navbar__link-wrapper").classList.remove("animate__slideInRight");
+    document.querySelector(".navbar__link-wrapper").classList.add("animate__slideOutRight");
+});
+
 function openMenu() {
     let open = document.querySelector("#navDropdownMenu");
     open.classList.add("menu");
-}
-
-function closeMenu() {
-    let close = document.querySelector("#navDropdownMenu");
-    close.classList.remove("menu");
 }
